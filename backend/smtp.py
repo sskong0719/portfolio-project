844612a51a41a6ec99baa2e3b589d118b8a3e1dd
SamuelKong <63858847+sskong0719@users.noreply.github.com>
2023-11-06 02:00:10 -0500

    Update smtp.py

--- backend/smtp.py
+++ backend/smtp.py
@@ -1,16 +1,14 @@
 import smtplib
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 def send_email(name, email, message):
     # SMTP configuration
     smtp_server = 'smtp.gmail.com'
     # SSL 465, TLS 587
     smtp_port = 587 
-    smtp_username = os.getenv('SMTP_USERNAME')
-    smtp_password = os.getenv('SMTP_PASSWORD')
+    smtp_username = 'skportfolioproject@gmail.com'
+    smtp_password = 'fpxntsnrdjzvwvrh'
 
     sender = 'skportfolioproject@gmail.com'
     recipient = 'samuelkong990719@gmail.com'
