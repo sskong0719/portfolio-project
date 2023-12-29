830f5a44f6537a31a7f1fd031148d9c30d860887
ksy990719 <ksy990719@hotmail.com>
2023-12-29 14:54:08 -0500

    Update smtp.py

--- backend/smtp.py
+++ backend/smtp.py
@@ -1,5 +1,7 @@
 import smtplib
 import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,25 +9,28 @@ load_dotenv()
 def send_email(name, email, message):
     # SMTP configuration
     smtp_server = 'smtp.gmail.com'
-    # SSL 465, TLS 587
-    smtp_port = 465
+    smtp_port = 465  # SSL port
     smtp_username = os.getenv('SMTP_USERNAME')
     smtp_password = os.getenv('SMTP_PASSWORD')
     sender = 'skportfolioproject@gmail.com'
     recipient = 'samuelkong990719@gmail.com'
 
+    # Email subject
     subject = '[SKPP] New message from {} ({})'.format(name, email)
+
+    # Create a multipart message and set headers
+    msg = MIMEMultipart()
+    msg['From'] = sender
+    msg['To'] = recipient
+    msg['Subject'] = subject
+
+    # Add body to email with UTF-8 encoding
     body = f"Name: {name}\nEmail: {email}\nMessage: {message}"
+    msg.attach(MIMEText(body, 'plain', 'utf-8'))
 
     # Connect to the SMTP server
-    server = smtplib.SMTP_SSL(smtp_server, smtp_port)
-    server.login(smtp_username, smtp_password)
+    with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
+        server.login(smtp_username, smtp_password)
+        server.send_message(msg)  # send_message automatically encodes the message
 
-    # Compose the email
-    email_message = f"Subject: {subject}\n\n{body}"
-
-    # Send the email
-    server.sendmail(sender, recipient, email_message)
-
-    # Disconnect from the SMTP server
-    server.quit()
+    # No need to explicitly quit with the 'with' statement
