948eaf3a383194c731da496698d4f7beed78661e
ksy990719 <ksy990719@hotmail.com>
2024-06-13 08:32:54 -0400

    bug fix

--- backend/collections/contact_collection.py
+++ backend/collections/contact_collection.py
@@ -1,11 +1,10 @@
-from pymongo import MongoClient
 import datetime
 import pytz
-
+from pymongo.collection import Collection
 
 class ContactCollection:
     def __init__(self, db):
-        self.contact_collection = db["Contact"]
+        self.contact_collection: Collection = db["Contact"]
 
     def add_contact(self, name, email, message):
         timezone = pytz.timezone("US/Eastern")
@@ -13,7 +12,8 @@ class ContactCollection:
 
         try:
             existing_contact = self.contact_collection.find_one(
-                {"email": email}, {"_id": 0}
+                {"email": email},
+                {"_id": 0}
             )
             if existing_contact:
                 messages = existing_contact.get("messages", [])
@@ -22,10 +22,14 @@ class ContactCollection:
                     "datetime": current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
                 }
                 messages.append(new_message)
-                self.contact_collection.update_one(
-                    {"email": email}, {"$set": {"messages": messages}}
+                result = self.contact_collection.update_one(
+                    {"email": email},
+                    {"$set": {"messages": messages}}
                 )
-                return True
+                if result.modified_count > 0:
+                    return True
+                else:
+                    return False
             else:
                 new_contact = {
                     "name": name,
@@ -33,9 +37,9 @@ class ContactCollection:
                     "messages": [
                         {
                             "message": message,
-                            "datetime": current_datetime.strftime("%Y-%m-%d %H:%M:%S"),
+                            "datetime": current_datetime.strftime("%Y-%m-%d %H:%M:%S")
                         }
-                    ],
+                    ]
                 }
                 self.contact_collection.insert_one(new_contact)
                 return True
--- backend/database.py
+++ backend/database.py
@@ -1,17 +1,15 @@
 from pymongo import MongoClient
 import os
-
 from dotenv import load_dotenv
 from collections.contact_collection import ContactCollection
 from collections.project_collection import ProjectCollection
 
 load_dotenv()
 
-
 class Database:
     def __init__(self):
-        MONGO_INITDB_ROOT_USERNAME = os.getenv("MONGO_INITDB_ROOT_USERNAME")
-        MONGO_INITDB_ROOT_PASSWORD = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+        MONGO_INITDB_ROOT_USERNAME = os.getenv('MONGO_INITDB_ROOT_USERNAME')
+        MONGO_INITDB_ROOT_PASSWORD = os.getenv('MONGO_INITDB_ROOT_PASSWORD')
         connection_string = f"mongodb://{MONGO_INITDB_ROOT_USERNAME}:{MONGO_INITDB_ROOT_PASSWORD}@mongo:27017/?authSource=admin"
         mongo_client = MongoClient(connection_string)
         self.db = mongo_client["Portfolio-Website"]
