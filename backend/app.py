0a58f524c78db98fbf2a0eea95ef09b766b00b1a
ksy990719 <ksy990719@hotmail.com>
2024-06-15 00:59:57 -0400

    update

--- backend/app.py
+++ backend/app.py
@@ -33,14 +33,16 @@ db = Database()
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
-@app.route("/", methods=["GET"])
-def root():
-    logger.debug(f"Root reached")
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
+    logger.debug("catch_all function called")
     user_id = request.cookies.get("user_id")
+    logger.debug(f"Current user_id cookie: {user_id}")
     if not user_id:
         user_id = str(uuid.uuid4())
         logger.debug(f"Generated new user_id: {user_id}")
-        response = make_response("User ID set", 200)
+        response = make_response(send_from_directory(app.static_folder, "index.html"))
         response.set_cookie(
             "user_id",
             user_id,
@@ -51,10 +53,11 @@ def root():
         )
         logger.info(f"Set-Cookie header: {response.headers.get('Set-Cookie')}")
         db.visits_collection.increment_unique_visit_count()
-        return response
     else:
         logger.debug("User already has a user_id cookie")
-        return "User ID exists", 200
+        response = make_response(send_from_directory(app.static_folder, "index.html"))
+
+    return response
 
 
 @app.route("/api/visit-count", methods=["GET"])
